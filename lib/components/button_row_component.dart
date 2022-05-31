072bb0576785f09f9216dbfeb016f541d67137df
joaoffnogueira <contact@joaonogueira.dev>
2022-05-31 20:52:51 -0300

    Create row component

---
+++ lib/components/button_row_component.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:purple_calc/components/button_component.dart';
+
+class ButtonRowComponent extends StatelessWidget {
+  final List<ButtonComponent> buttons;
+  const ButtonRowComponent({required this.buttons, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: buttons,
+      ),
+    );
+  }
+}
