316195b0269f4b2f5054ea47231aa338497b5666
joaoffnogueira <contact@joaonogueira.dev>
2022-06-01 16:44:48 -0300

    Sending the input to memoryModel

--- lib/models/memory_model.dart
+++ lib/models/memory_model.dart
@@ -1,4 +1,34 @@
 class MemoryModel {
-  MemoryModel();
+  String _value = '0';
+
+  String get value => _value;
+
+  void allClear() {
+    _value = '0';
+  }
+
+  void applyCommand(String comand) {
+    if (comand == 'AC') {
+      allClear();
+    } else if (comand == '+/-') {
+      _value = _value.startsWith('-') ? _value.substring(1) : '-$_value';
+    } else if (comand == '%') {
+      _value = (_value == '0')
+          ? '0'
+          : '${(double.parse(_value) / 100).toStringAsFixed(2)}';
+    } else if (comand == '.') {
+      if (_value.contains('.')) {
+        return;
+      }
+      _value += '.';
+    } else if (comand == '=') {
+      //_value = eval(_value).toString();
+    } else {
+      if (_value == '0') {
+        _value = comand;
+      } else {
+        _value += comand;
+      }
+    }
+  }
 }
-  
--- lib/screens/calc_screen.dart
+++ lib/screens/calc_screen.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:purple_calc/components/display_component.dart';
 import 'package:purple_calc/components/keyboard_component.dart';
+import 'package:purple_calc/models/memory_model.dart';
 
-class CalcScreen extends StatelessWidget {
-  _onPressed(String text) {
-    print(text);
-  }
-
+class CalcScreen extends StatefulWidget {
   const CalcScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalcScreen> createState() => _CalcScreenState();
+}
+
+class _CalcScreenState extends State<CalcScreen> {
+  final MemoryModel memory = MemoryModel();
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
         children: [
-          DisplayComponent(text: 'Display'),
+          DisplayComponent(text: memory.value),
           KeyboardComponent(onPressed: _onPressed),
         ],
       ),
